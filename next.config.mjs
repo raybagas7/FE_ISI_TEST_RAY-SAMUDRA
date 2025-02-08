9e17a13b949fb1f22c6615331fdae82d6f4951d3
raybagas7 <raysam.gas@gmail.com>
2025-02-08 13:34:43 +0700

    first setup: initialize docker

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000, // Check for file changes every second
+      aggregateTimeout: 300, // Delay before rebuilding
+    };
+    return config;
+  },
+  output: 'standalone',
+};
 
 export default nextConfig;
