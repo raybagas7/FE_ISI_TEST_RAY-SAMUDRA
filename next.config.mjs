213b6dd0601946382a3f6341579ab3c369ea1575
raybagas7 <raysam.gas@gmail.com>
2025-02-12 22:48:51 +0700

    fix: remove webpack from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpackDevMiddleware: (config) => {
-    // config.watchOptions = {
-    //   poll: 1000, // Check for file changes every second
-    //   aggregateTimeout: 300, // Delay before rebuilding
-    // };
-    return config;
-  },
   output: 'standalone',
 };
 
