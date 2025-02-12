50a46c3457ba004517fb335e094ad0b22bb38139
raybagas7 <raysam.gas@gmail.com>
2025-02-12 23:39:33 +0700

    chore: add dotenc to drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,9 @@
 import { defineConfig } from 'drizzle-kit';
+import dotenv from 'dotenv';
+
+dotenv.config({
+  path: '.env',
+});
 
 export default defineConfig({
   schema: './src/lib/db/schema.ts',
